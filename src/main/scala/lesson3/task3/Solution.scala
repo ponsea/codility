147b263f9bcd436f11f679e87f508e37593b5752
ponsea <puodsea@gmail.com>
2019-04-21 21:22:43 +0900

    Lesson 3-3 TapeEquilibrium

---
+++ src/main/scala/lesson3/task3/Solution.scala
@@ -0,0 +1,15 @@
+package lesson3.task3
+
+object Solution {
+  def solution(a: Array[Int]): Int = {
+    val sumsFromLeft = a.tail.foldLeft(IndexedSeq(a.head)) { (acc, cur) =>
+      acc :+ (acc.last + cur)
+    }
+    val sumsFromRight = a.init.foldRight(IndexedSeq(a.last)) { (cur, acc) =>
+      (cur + acc.head) +: acc
+    }
+    (sumsFromLeft.init zip sumsFromRight.tail)
+      .map{ case (leftSum, rightSum) => math.abs(leftSum - rightSum) }
+      .min
+  }
+}
