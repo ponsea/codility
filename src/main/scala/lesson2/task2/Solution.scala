e83c30868a7a9700e27b7ec678e448edf9c9923d
ponsea <puodsea@gmail.com>
2019-04-30 15:16:23 +0900

    Lesson 2-2 OddOccurrencesInArray 効率的な解答に変更

--- src/main/scala/lesson2/task2/Solution.scala
+++ src/main/scala/lesson2/task2/Solution.scala
@@ -1,11 +1,11 @@
 package lesson2.task2
 
+// Report: https://app.codility.com/demo/results/training5U6XVD-T44/
+
 object Solution {
   def solution(a: Array[Int]): Int = {
-    a.foldLeft(Map.empty[Int, Int]) { (acc, cur) =>
-      acc + (cur -> acc.get(cur).fold(1)(_ + 1))
-    }.collectFirst{
-      case (key, count) if count % 2 == 1 => key
-    }.get
+    a.foldLeft(Set.empty[Int]) { (acc, cur) =>
+      if (acc.contains(cur)) acc - cur else acc + cur
+    }.head
   }
 }
